04a018329c9f8b265a60c1fae032fd7cd406bbe9
Nailah Azzahra <145175831+nailahazhr@users.noreply.github.com>
2024-11-20 04:49:59 +0700

    Create product_data.dart

---
+++ lib/constants/product_data.dart
@@ -0,0 +1,14 @@
+import 'package:ns_apps/model/product_model.dart';
+
+List<GiziProduk> giziProdukList = [
+  GiziProduk(
+    title: "Nasi Putih",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/2/2d/Nasi_dibentuk_bulat.jpg", 
+    kalori: "180 kkal",
+    karbohidrat: "39.8 g",
+    protein: "3 g",
+    lemak: "0.3 g",
+    gula: "0.3 g",
+    garam: "1 mg",
+  ),
+];
