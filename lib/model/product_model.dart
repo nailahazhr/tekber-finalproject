6aaa973fac69171370b45868ddbb23ec012ee4cf
Nailah Azzahra <145175831+nailahazhr@users.noreply.github.com>
2024-11-20 04:49:33 +0700

    Create product_model.dart

---
+++ lib/model/product_model.dart
@@ -0,0 +1,21 @@
+class GiziProduk {
+  final String title;
+  final String imageUrl;
+  final String kalori;
+  final String karbohidrat;
+  final String protein;
+  final String lemak;
+  final String gula;
+  final String garam;
+
+  GiziProduk({
+    required this.title,
+    required this.imageUrl, 
+    required this.kalori,
+    required this.karbohidrat,
+    required this.protein,
+    required this.lemak,
+    required this.gula,
+    required this.garam,
+  });
+}
