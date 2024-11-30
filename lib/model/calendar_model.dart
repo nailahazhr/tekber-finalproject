fd627421c91919296b2f399a38cd98e375820a61
Nailah Azzahra <145175831+nailahazhr@users.noreply.github.com>
2024-11-30 13:17:29 +0700

    Update calendar_model.dart

--- lib/model/calendar_model.dart
+++ lib/model/calendar_model.dart
@@ -1,6 +1,20 @@
+import 'package:flutter/material.dart';
+
 class Batas {
   final String jenis;
   final double batas;
 
   Batas({required this.jenis, required this.batas});
+}
+
+class ChartData {
+  final String jenis;  
+  final double konsumsi;  
+  final Color warna; 
+
+  ChartData({
+    required this.jenis, 
+    required this.konsumsi, 
+    required this.warna,
+  });
 }
