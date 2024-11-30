a38cc17de2ef536caeacc9fad69a7dcc897240e9
Nailah Azzahra <145175831+nailahazhr@users.noreply.github.com>
2024-11-30 08:39:08 +0700

    Update calendar_screen.dart

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import '../constants/colors.dart';
 import 'package:table_calendar/table_calendar.dart';
 import '../model/calendar_model.dart';
+import '../constants/calendar_data.dart'; // Import file data
 
 class CalendarScreen extends StatefulWidget {
   CalendarScreen({Key? key}) : super(key: key);
@@ -11,33 +12,11 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  // Status untuk pilihan tampilan Harian atau Bulanan
   bool _isHarianSelected = true;
 
-  // Konfigurasi kalender
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
-
-  // Tanggal yang ingin ditandai
-  final _holidayDates = [
-    DateTime(2024, 11, 18),
-    DateTime(2024, 11, 20),
-  ];
-
-  // Data batas maksimal (contoh data)
-  final Map<DateTime, List<Batas>> batasMaksimal = {
-    DateTime(2024, 11, 18): [
-      Batas(jenis: "Lemak", batas: 65.0),
-      Batas(jenis: "Gula", batas: 50.0),
-      Batas(jenis: "Garam", batas: 5.0),
-    ],
-    DateTime(2024, 11, 20): [
-      Batas(jenis: "Lemak", batas: 70.0),
-      Batas(jenis: "Gula", batas: 45.0),
-      Batas(jenis: "Garam", batas: 6.0),
-    ],
-  };
 
   void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
     setState(() {
@@ -50,6 +29,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      // Header
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(
@@ -77,7 +57,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
     );
   }
 
-  // Widget untuk bagian pilihan Harian atau Bulanan
+  // Sections
   Widget _buildSections() {
     return Row(
       children: [
@@ -145,14 +125,12 @@ class _CalendarScreenState extends State<CalendarScreen> {
     );
   }
 
-  // Widget untuk konten Harian
   Widget _buildHarianContent() {
     return Center(
       child: Text("Konten Harian"),
     );
   }
 
-  // Widget untuk konten Bulanan
   Widget _buildBulananContent() {
     return Column(
       children: [
@@ -175,6 +153,7 @@ class _CalendarScreenState extends State<CalendarScreen> {
                     style: const TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
+                      color: tThirdColor,
                     ),
                   ),
                   SizedBox(height: 12),
@@ -197,72 +176,80 @@ class _CalendarScreenState extends State<CalendarScreen> {
     );
   }
 
-  // Widget untuk kalender
   Widget _Calendar() {
-    return TableCalendar(
-      rowHeight: 50,
-      headerStyle: HeaderStyle(
-        formatButtonVisible: false,
-        titleCentered: true,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0), // Tambahkan jarak ke kanan dan kiri
+      child: TableCalendar(
+        rowHeight: 50,
+        headerStyle: HeaderStyle(
+          formatButtonVisible: false,
+          titleCentered: true,
+        ),
+        availableGestures: AvailableGestures.all,
+        selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
+        focusedDay: _focusedDay,
+        firstDay: DateTime.utc(2020, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        onDaySelected: _onDaySelected,
+        calendarStyle: CalendarStyle(
+          selectedDecoration: BoxDecoration(
+            color: tThirdColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          todayDecoration: BoxDecoration(
+            color: tAccentColor2,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          holidayTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+          holidayDecoration: BoxDecoration(
+            color: tAccentColor2,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        holidayPredicate: (day) {
+          return holidayDates.any((holiday) => isSameDay(holiday, day));
+        },
+        eventLoader: (day) {
+          return [];
+        },
       ),
-      availableGestures: AvailableGestures.all,
-      selectedDayPredicate: (day) => isSameDay(day, _selectedDay),
-      focusedDay: _focusedDay,
-      firstDay: DateTime.utc(2020, 10, 16),
-      lastDay: DateTime.utc(2030, 3, 14),
-      onDaySelected: _onDaySelected,
-      calendarStyle: CalendarStyle(
-        selectedDecoration: BoxDecoration(
-          color: tThirdColor,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        todayDecoration: BoxDecoration(
-          color: tAccentColor2,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        holidayDecoration: BoxDecoration(
-          color: tAccentColor2,
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
-      eventLoader: (day) {
-        if (_holidayDates.any((holiday) => isSameDay(holiday, day))) {
-          return [""];
-        }
-        return [];
-      },
     );
   }
 
-  // Widget untuk card batas maksimal
+
   Widget _buildInfoCard(Batas batas) {
     return Card(
       elevation: 3,
+      color: const Color.fromARGB(255, 248, 248, 248),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(12),
       ),
       child: Container(
-        width: 150,
-        height: 180,
-        padding: EdgeInsets.only(left:(10), right:(10), top:(24), bottom:(24)),
+        // Gunakan MediaQuery untuk mengambil lebar layar
+        width: MediaQuery.of(context).size.width * 0.3, 
+        padding: EdgeInsets.all(16),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
-              "Konsumsi ${batas.jenis}",
+              "Konsumsi"+"\n${batas.jenis}",
+              textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 16,
                 fontWeight: FontWeight.bold,
                 color: tThirdColor,
               ),
             ),
-            SizedBox(height: 4),
+            SizedBox(height: 8),
             Text(
               "Batas Konsumsi ${batas.jenis} Harian Anda Maksimal (gram):",
-              style: TextStyle(fontSize: 12, color: Colors.grey[600]),
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12, color: const Color.fromARGB(255, 48, 48, 48)),
             ),
-            SizedBox(height: 4),
+            SizedBox(height: 8),
             Row(
               mainAxisSize: MainAxisSize.min,
               mainAxisAlignment: MainAxisAlignment.center,
@@ -284,4 +271,5 @@ class _CalendarScreenState extends State<CalendarScreen> {
       ),
     );
   }
+
 }
