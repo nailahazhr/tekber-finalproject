7ec9468815a8e4d873ab533778ec0b853af2f8f8
Ahdaf <mahdaf8@gmail.com>
2024-12-08 12:25:25 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -8,12 +8,12 @@ import 'package:ns_apps/screens/articles.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+
   runApp(const MyApp());
 }
 
