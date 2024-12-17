e3c16fc731fe082b47c514a8c718cdf33db79c50
Nailah Azzahra <145175831+nailahazhr@users.noreply.github.com>
2024-12-17 12:17:59 +0700

    Update calendar_screen.dart

--- lib/screens/calendar_screen.dart
+++ lib/screens/calendar_screen.dart
@@ -1,9 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_charts/charts.dart';
 import '../constants/colors.dart';
-import 'package:table_calendar/table_calendar.dart';
-import '../model/calendar_model.dart';
-import '../constants/calendar_data.dart';
 import 'package:easy_date_timeline/easy_date_timeline.dart';
 
 class CalendarScreen extends StatefulWidget {
@@ -14,20 +11,12 @@ class CalendarScreen extends StatefulWidget {
 }
 
 class _CalendarScreenState extends State<CalendarScreen> {
-  final CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _selectedDay = DateTime.now();
-
-  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
-    setState(() {
-      _selectedDay = selectedDay;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      // Header
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: const Text(
@@ -39,95 +28,225 @@ class _CalendarScreenState extends State<CalendarScreen> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          _buildHarianContent()
-        ],
+      body: SingleChildScrollView( // SingleChildScrollView 
+        child: Column( // Bungkus Column dengan widget container
+          children: [
+            _buildHarianContent()
+          ],
+        ),
       ),
     );
   }
 
   Widget _buildHarianContent() {
-    return Column(
+    DateTime startOfDay = DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day);
+    DateTime endOfDay = startOfDay.add(const Duration(days: 1)).subtract(const Duration(microseconds: 1));
+
+    return  Column(
       children: [
         const SizedBox(height: 24),
         EasyDateTimeLine(
-          initialDate: DateTime.now(),
+          initialDate: _selectedDay, 
           onDateChange: (selectedDate) {
             setState(() {
-              _selectedDay = selectedDate;
+              _selectedDay = selectedDate; 
             });
           },
           activeColor: tSecondaryColor,
         ),
         const SizedBox(height: 20),
-        batasMaksimal.containsKey(
-                DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day))
-            ? Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Divider(
-                    color: Colors.grey.shade100,
-                    thickness: 1,
-                    indent: 20,
-                    endIndent: 20,
-                  ),
-                  const SizedBox(height: 12),
-                  const Center(
-                    child: Text(
-                      "Konsumsi Nutrisi Harian",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: tThirdColor,
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+            .collection('nutrisiPengguna')
+            .where('time', isGreaterThanOrEqualTo: DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day))
+            .where('time', isLessThan: DateTime(_selectedDay.year, _selectedDay.month, _selectedDay.day).add(const Duration(days: 1)).subtract(const Duration(microseconds: 1)))
+            .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+
+            final docs = snapshot.data!.docs;
+
+            if (docs.isEmpty) {
+              return const Center(child: Text('No data available for today.'));
+            }
+
+            return FutureBuilder<Map<String, dynamic>>(
+              future: _fetchNutritionDetails(docs), // Fetch referenced data
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                }
+
+                final nutritionData = snapshot.data ?? {};
+
+                // Current values
+                double carbs = nutritionData["karbohidrat"] ?? 0;
+                double fat = nutritionData["lemak"] ?? 0;
+                double protein = nutritionData["protein"] ?? 0;
+                double calories = nutritionData["kalori"] ?? 0;
+                double sugar = nutritionData["gula"] ?? 0;
+                double salt = nutritionData["garam"] ?? 0;
+
+                // Target values
+                const double carbsTarget = 200;
+                const double fatTarget = 59;
+                const double proteinTarget = 80;
+                const double caloriesTarget = 2159;
+                const double sugarTarget = 50;
+                const double saltTarget = 8;
+
+                return Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      const Divider(
+                        color: Color.fromARGB(255, 238, 238, 238), 
+                        thickness: 1,
                       ),
-                    ),
+                      const SizedBox(height: 8),
+                      const Text('Konsumsi Nutrisi Harian',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: tThirdColor)),
+                      const SizedBox(height: 20),
+                      Row( // Baris pertama
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _buildCircularStat(
+                              currentValue: calories,
+                              targetValue: caloriesTarget,
+                              label: 'kalori'),
+                          _buildCircularStat(
+                              currentValue: carbs,
+                              targetValue: carbsTarget,
+                              label: 'karbohidrat'),
+                          _buildCircularStat(
+                              currentValue: fat,
+                              targetValue: fatTarget,
+                              label: 'lemak'),
+                        ],
+                      ),
+                      const SizedBox(height: 20), // Jarak antar baris
+                      Row( // Baris kedua
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _buildCircularStat(
+                              currentValue: protein,
+                              targetValue: proteinTarget,
+                              label: 'protein'),
+                          _buildCircularStat(
+                              currentValue: sugar,
+                              targetValue: sugarTarget,
+                              label: 'gula'),
+                          _buildCircularStat(
+                              currentValue: salt,
+                              targetValue: saltTarget,
+                              label: 'garam'),
+                        ],
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 12),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: batasMaksimal[DateTime(
-                            _selectedDay.year,
-                            _selectedDay.month,
-                            _selectedDay.day)]!
-                        .map((batas) => _nutrisiCard(batas))
-                        .toList(),
-                  ),
-                ],
-              )
-            : const Center(
-                child: Text("Tidak ada informasi untuk tanggal ini."),
-              ),
-        const SizedBox(height: 24),
-      ],
+                );
+              },
+            );
+          },
+        )
+      ]
     );
   }
 
-  Widget _nutrisiCard(Batas batas) {
-    return Column(
-      children: [
-        SizedBox(
-          width: MediaQuery.of(context).size.width * 0.98,
-          height: 100, 
-          child: SfCartesianChart(
-            borderWidth: 0,
-            plotAreaBorderWidth: 0,
-            primaryXAxis: CategoryAxis(
-              majorGridLines: const MajorGridLines(width: 0),
-              labelStyle: const TextStyle(
-                fontWeight: FontWeight.bold,
+  Future<Map<String, dynamic>> _fetchNutritionDetails(List<QueryDocumentSnapshot> docs) async {
+    Map<String, dynamic> aggregatedData = {
+      "karbohidrat": 0,
+      "lemak": 0,
+      "protein": 0,
+      "kalori": 0,
+      "gula": 0,
+      "garam": 0,
+    };
+
+    for (var doc in docs) {
+      // Mengambil DocumentReference dari field 'id'
+      DocumentReference makananRef = doc.get('id');
+      DocumentSnapshot detailSnapshot = await makananRef.get();
+
+      if (detailSnapshot.exists) {
+        var detailData = detailSnapshot.data() as Map<String, dynamic>;
+
+        // Aggregate nutritional values
+        aggregatedData["karbohidrat"] += detailData["karbohidrat"] ?? 0;
+        aggregatedData["lemak"] += detailData["lemak"] ?? 0;
+        aggregatedData["protein"] += detailData["protein"] ?? 0;
+        aggregatedData["kalori"] += detailData["kalori"] ?? 0;
+        aggregatedData["gula"] += detailData["gula"] ?? 0;
+        aggregatedData["garam"] += detailData["garam"] ?? 0;
+      }
+    }
+
+    return aggregatedData;
+  }
+
+  Widget _buildCircularStat({
+  required double currentValue,
+  required double targetValue,
+  required String label,
+}) {
+  double progress = currentValue / targetValue;
+  progress = progress.clamp(0.0, 1.0);
+
+  // Tentukan warna berdasarkan progress
+  Color indicatorColor;
+  if (progress >= 0.75) {
+    indicatorColor = const Color.fromARGB(255, 239, 99, 81); // Merah jika >= 75%
+  } else if (progress >= 0.5) {
+    indicatorColor = const Color.fromARGB(255, 239, 194, 81); // Kuning jika >= 50%
+  } else {
+    indicatorColor = const Color.fromARGB(255, 41, 150, 44); // Hijau jika < 50%
+  }
+
+    return Card(
+      color: Colors.white,
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.25, // Perbesar width
+        height: 150, // Perbesar height
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  CircularProgressIndicator(
+                    value: progress,
+                    backgroundColor: Colors.grey[200],
+                    valueColor: AlwaysStoppedAnimation<Color>(indicatorColor),
+                    strokeWidth: 10, // Perbesar strokeWidth
+                  ),
+                ],
               ),
-            ),
-            primaryYAxis: NumericAxis(
-              majorGridLines: const MajorGridLines(width: 0),
-              axisLine: const AxisLine(width: 0),
-              isVisible: false,
-            ),
-            series: buildChartSeries(_selectedDay, batas), 
-            tooltipBehavior: TooltipBehavior(enable: true),
+              const SizedBox(height: 16),
+              Text(
+                '$currentValue/$targetValue',
+                style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+              ),
+              Text(label),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
